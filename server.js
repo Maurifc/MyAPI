389a44af8003d64c9d42819a9c06f1392d942577
maurifcarmo <mauri_vrb@hotmail.com>
2022-03-19 16:36:25 -0300

    Remove db connection test

--- server.js
+++ server.js
@@ -6,11 +6,4 @@ const port = 3000
 // Routes
 app.use('/user', userRouter) // Forward every /user route to userRouter
 
-try {
-    sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-
 app.listen(port, () => console.log('App is listening on port ' + port))

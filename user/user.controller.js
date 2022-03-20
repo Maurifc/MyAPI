3b306830534668d609c00c48b4a47d1ba83fb7c2
maurifcarmo <mauri_vrb@hotmail.com>
2022-03-20 17:21:42 -0300

    Add another methods for /user endpoint
    - Add select user by ID
    - Add update user
    - Add delete user
    - Add field validation function

--- user/user.controller.js
+++ user/user.controller.js
@@ -1,5 +1,18 @@
 const User = require('./user.model')
 
+
+// TODO: Move to another file
+function validate(userData){
+    const field = ['userName', 'password', 'bornDate']
+
+    field.forEach((field) => {
+        const value = userData[field]
+
+        if(typeof value !== 'string' || value.length === 0)
+            throw new Error('Invalid field ' + field)
+    })
+}
+
 module.exports = {
     list: async (req, res) => {
         const users = await User.findAll({ raw: true})
@@ -8,15 +21,78 @@ module.exports = {
         res.json(users)
     },
 
+    getUserById: async(req, res) => {
+        try {
+            const userId = req.params.userId
+            const user = await User.findByPk(userId)
+
+            if(user === null)
+                throw new Error('Invalid user ID')
+
+            res.json(user)
+        } catch (error) {
+            res.status(404)
+            res.send()
+        }
+    },
+
     create: (req, res) => {
-        const data = req.body
+        try {
+            const data = req.body
 
-        User.create({
-            userName: data.userName,
-            password: data.password,
-            bornDate: data.bornDate,
-        })
+            validate(data)
 
-        res.send(201)
+            User.create({
+                userName: data.userName,
+                password: data.password,
+                bornDate: data.bornDate,
+            })
+    
+            res.send(201)            
+        } catch (error) {
+            res.status(400).json({ error: error.message })
+        }
+    },
+
+    update: async (req, res) => {
+        try {
+            const userId = req.params.userId
+            const data = req.body
+            const user = await User.findByPk(userId)
+
+            if(user === null)
+                throw new Error('Invalid user ID')
+                        
+            await User.update(data, {
+                where: {
+                    id: userId
+                }
+            })
+
+            res.status(204).send()
+        } catch (error) {
+            res.status(400).send()
+        }
+    },
+
+    delete: async(req, res) => {
+        try {
+            const userId = req.params.userId
+            const user = await User.findByPk(userId)
+    
+            if(user === null)
+                throw new Error('Invalid user ID')
+    
+            await User.destroy({
+                where: {
+                   id: userId 
+                }
+            })
+    
+            res.status(204).send()            
+        } catch (error) {
+            res.status(404).send()            
+        }
     }
+
 }
--- user/user.router.js
+++ user/user.router.js
@@ -3,6 +3,9 @@ const userController = require('./user.controller')
 
 
 router.get('/', userController.list)
+router.get('/:userId', userController.getUserById)
 router.post('/', userController.create)
+router.put('/:userId', userController.update)
+router.delete('/:userId', userController.delete)
 
 module.exports = router
